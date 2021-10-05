ea1e30dc033fe998a56aaa33bff5242a2242b2e3
EleoEris <79694371+EleoEris@users.noreply.github.com>
2021-10-05 18:50:54 +0200

    Added help triggers

--- my_cmd.bat
+++ my_cmd.bat
@@ -1,2 +1,30 @@
 @echo off
-python "%USERPROFILE%/Desktop/Small Projects/custom cmd/scripts/%1.py" %*
+IF "%1" == "" (
+  goto help
+) ELSE (
+  IF "%1" == "/?" (
+    goto help
+  ) ELSE (
+    IF "%1" == "-h" (
+      goto help
+    ) ELSE (
+      IF "%1" == "--help" (
+        goto help
+      ) ELSE (
+        python "%USERPROFILE%/Desktop/Small Projects/custom cmd/scripts/%1.py" %*
+        goto end
+      )
+    )
+  )
+)
+
+:help
+  echo Hello! Type the name of a script.
+  echo.
+  echo Available scripts:
+  for %%f in ("%~dp0scripts\*.*") do (
+    echo - %%~nf
+  )
+
+:end
+  echo.
