75501a2fda6a66f15b5bf50c20d0f0630003b54a
EleoEris <79694371+EleoEris@users.noreply.github.com>
2021-10-05 17:13:53 +0200

    Added *git.py*

---
+++ scripts/git.py
@@ -0,0 +1,61 @@
+import sys
+from requests import get
+
+raw  = "https://raw.githubusercontent.com/EleoEris/custom-cmd/main/README.md"
+norm = "https://github.com/EleoEris/custom-cmd/blob/main/README.md"
+
+def print_help():
+        print("Useful git commands")
+        print("clone1 link path         - clones a single file from github")
+        print("                         - link - mandatory - link to the file")
+        print("                         - path - where to store the file")
+        print("clone1 user repo file    - details of repository")
+
+def main(cmd):
+    path = ""
+    
+    if len(cmd) == 0 or cmd[0] == '--help' or cmd[0] == '-h' or cmd[0] == '/?':
+        print_help()
+        return(0)
+        
+    if cmd[0] == 'clone1':              ### Clone a single file from github
+                                        ### cmd[1] - link, (cmd[2] - path) or
+                                        ### cmd[1] - username, cmd[2] - repo, cmd[3] - branch, cmd[4] - file name, (cmd[5] - path)
+        if "github.com" in cmd[1]:      ### User has provided a link
+            link = cmd[1].replace("github.com", "raw.githubusercontent.com").replace("blob/", "")
+            try:
+                path = cmd[2]
+            except IndexError:  ### path was not given
+                pass
+        else:
+            try:
+                link = f"https://raw.githubusercontent.com/{cmd[1]}/{cmd[2]}/{cmd[3]}/{cmd[4]}"
+                try:            ### path was not given
+                    path = cmd[5]
+                except IndexError:
+                    pass
+            except IndexError:
+                print("Not a github link. Please provide a github link or (in order) the username, repository name, branch and file name")
+        print(locals())
+        if not "link" in locals():
+            print("Some error occurred!")
+            return(1)
+        print(link)
+        request = get(link)
+        print(f"TEXT:\n{req.text}\n==========================")
+        if request.status_code == 200:
+            if path:
+                with open(path) as output:
+                    output.write(request.text)
+                    print(f"Saved file successfully to {output.name}.")
+            else:
+                with open(link[link.rindex("/" + 1):]) as output:
+                    output.write(request.text)
+                    print(f"Saved file successfully to {output.name}.")
+        else:
+            print(f"Couldn't get file from link. Status code: {request.status_code}.\n Please check your input or try visiting the link yourself.\n{link}")
+
+
+if __name__ == '__main__':
+    print(f"Arguments: {sys.argv[2:]}")
+    main(sys.argv[2:])
